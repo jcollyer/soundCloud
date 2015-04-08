e4ed717c13e38d20171a348c4067071a7e1a516a
jcollyer <collyerdesign@gmail.com>
2015-04-07 18:57:12 -0700

    add proper load for soundCloud API

--- js/app.js
+++ js/app.js
@@ -1,81 +1,63 @@
-SC.initialize({
-  client_id: "be2f745f816c1df784b23dc87a1fd65f",
-  redirect_uri: "file:///Users/jcollyer/Documents/projects/soundCloud/index.html",
-});
-
-// SC.get("/groups/55517/tracks", {limit: 1}, function(tracks){
-//   alert("Latest track: " + tracks[0].title);
-// });
-
-
-// SC.get('/tracks/293', function(track){
-//   SC.oEmbed(track.permalink_url, document.getElementById('player'));
-// })
-
 var toggle = document.getElementById('toggle');
 var tracks = document.getElementsByClassName("tracks");
 var results = document.getElementById('results');
 var target = document.getElementById('target');
 var playlist = [];
-var iframe = document.getElementById('soundcloud_widget');
-var player = SC.Widget(iframe);
+
+// according to docs: https://developers.soundcloud.com/docs/api/html5-widget
+var iframe   = document.querySelector('iframe');
+var iframeID = iframe.id;
+var player   = SC.Widget(iframe);
+var player2  = SC.Widget(iframeID);
+// widget1 === widget2
 
 
-
-// function playSomeSound(genre){
-//   SC.get("/tracks", {
-//     genres: genre,
-//     bmp: {
-//       from: 100
-//     }
-//   }, function(tracks) {
-//     var random = Math.floor(Math.random() * 49);
-//     SC.oEmbed(tracks[random].uri, { auto_play: true}, document.getElementById('target'));
-//   })
-// }
-
+// var iframe = document.getElementById('soundcloud_widget');
+// var player = SC.Widget(iframe);
 
 function showTrackList(genre) {
   SC.get('/tracks', { genres: genre }, function(tracks) {
-
-      tracks.forEach(function(track, index) {
-        // console.log(track);
-
-        results.innerHTML = results.innerHTML + '<li onclick="playTrack('+track.id+')"><img src="'+track.artwork_url+'" /><p>'+track.title+'</p>  </li>';
-
-        playlist.push(track.id);
-        console.log(track.id)
-      });
+    tracks.forEach(function(track, index) {
+      results.innerHTML = results.innerHTML + '<li onclick="playTrack('+track.id+')"><img src="'+track.artwork_url+'" /><p>'+track.title+'</p>  </li>';
+      playlist.push(track.id);
     });
-}
-
-function playTrack(id) {
-  var track = '/tracks/'+id;
-  console.log(track)
-  // SC.stream(track, function(sound) {
-  //   start.onclick = function(e) {
-  //     e.preventDefault();
-  //     sound.start();
-  //   };
-
-  //   stop.onclick = function(e) {
-  //     e.preventDefault();
-  //     sound.stop();
-  //   };
-  // });
-  // target.innerHTML = '<iframe id="soundcloud_widget" src="http://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/'+id+'&show_artwork=false&liking=false&sharing=false&auto_play=false" width="420" height="120" frameborder="no"></iframe>'
-  var src = 'http://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/'+id+'&show_artwork=false&liking=false&sharing=false&auto_play=false" width="420" height="120" frameborder="no"'
-  iframe.setAttribute("src", src);
-
-  player.bind(SC.Widget.Events.READY, function() {
-    // setInfo();
-    // player.play();
-    alert("ready!");
   });
 }
 
+function playTrack(id) {
+  // var src = 'http://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/'+id+'&show_artwork=false&liking=false&sharing=false&auto_play=false" width="420" height="120" frameborder="no"'
+  // iframe.setAttribute("src", src);
+  url = "https://api.soundcloud.com/tracks/"+id+"";
+  player.load(url, {
+    auto_play: true
+  });
+  player.bind(SC.Widget.Events.READY, function() {
+    playerReady();
+  });
 
+  player.bind(SC.Widget.Events.FINISH, function() {
+    console.log("track finished!");
+  });
 
+  player.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
+    console.log("progress: ");
+  });
+
+  // player.bind(SC.Widget.Events.LOAD_PROGRESS, function() {
+  //   console.log("loaded: " player.getLoaded());
+  // });
+}
+
+toggle.onclick = function(e) {
+  e.preventDefault();
+  player.toggle();
+};
+
+showList = function(genre) {
+  results.innerHTML = "";
+  console.log(genre);
+  showTrackList(genre);
+}
 
 window.onload = function() {
   SC.initialize({
@@ -83,23 +65,6 @@ window.onload = function() {
     // redirect_uri: "file:///Users/jcollyer/Documents/projects/soundCloud/index.html",
   });
 
-  var menuLinks = document.getElementsByClassName('genre');
-  var results = document.getElementById('results');
 
-
-  toggle.onclick = function(e) {
-    e.preventDefault();
-    player.toggle();
-  };
-
-  next.onclick = function(e) {
-
-  }
-
-  showList = function(genre) {
-    results.innerHTML = "";
-    console.log(genre);
-    showTrackList(genre);
-  }
 
 }//window.onload
